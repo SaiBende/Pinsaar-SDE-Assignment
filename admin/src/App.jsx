fee6de729e0aeb6a9036d4c5d79f1c11ed6d9f40
SaiBende <bendesai5703@gmail.com>
2025-08-21 14:34:57 +0530

    Adding Admin React Vite only Folder setup

---
+++ admin/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline  text-amber-300">
+        Hello world!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
